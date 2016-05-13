8f5b67b7e41b1f0dba91bb3397355686a2b841e3
achistya <achistya@uci.edu>
2016-05-12 17:12:39 -0700

    Size of heap will never grow

--- memlib.c
+++ memlib.c
@@ -12,7 +12,10 @@
 static char *mem_heap;     /* Points to first byte of heap */ 
 static char *mem_brk;      /* Points to last byte of heap plus 1 */
 static char *mem_max_addr; /* Max legal heap addr plus 1*/
-#define MAX_HEAP (20*(1<<20))  /* 20 MB */
+#define MAX_HEAP ((1<<9))  /* 20 MB */
+
+
+
 /* 
  * mem_init - Initialize the memory system model
  */
@@ -34,7 +37,7 @@ void *mem_sbrk(int incr)
 
     if ( (incr < 0) || ((mem_brk + incr) > mem_max_addr)) {
 	errno = ENOMEM;
-	fprintf(stderr, "ERROR: mem_sbrk failed. Ran out of memory...\n");
+	fprintf(stderr, "ERROR: Ran out of memory...\n");
 	return (void *)-1;
     }
     mem_brk += incr;
